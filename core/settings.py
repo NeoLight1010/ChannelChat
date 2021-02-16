da968c9bda2e1e29eaadb55954cf031be71dd26d
Anthony Suárez <neolight1010@gmail.com>
2021-02-16 13:07:36 -0500

    Set up Redis channel layer.

--- core/settings.py
+++ core/settings.py
@@ -130,6 +130,9 @@ STATICFILES_DIRS = [
 ASGI_APPLICATION = "core.routing.application"
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('localhost', 6379)],
+        },
     }
 }
